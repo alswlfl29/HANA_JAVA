c9144dfcdc27d5945bf1216d716cc60b7e19e656
alswlfl29 <budlalswl1129@gmail.com>
2024-03-13 10:22:02 +0900

    생성자, static 초기화 블록

---
+++ src/ex35.java
@@ -0,0 +1,25 @@
+// static 초기화 블럭
+class StaticClass {
+    int a;
+    static int b; // static int b = 0; // 암묵적으로 0으로 초기화됨
+
+    static { // 프로그램 시작 시(main 함수 이전에) 호출됨
+        b = 5;
+        System.out.println("static block!");
+    }
+
+    // 생성자 함수 - new를 통해 객체가 생성될 때 호출된다!
+    StaticClass() {
+        this.a = 3;
+        b = 10;
+        System.out.println("constructor block");
+    }
+}
+
+public class ex35 {
+    public static void main(String[] args) {
+        System.out.println(StaticClass.b); // 5
+        StaticClass s = new StaticClass();
+        System.out.println(StaticClass.b); // 10
+    }
+}
